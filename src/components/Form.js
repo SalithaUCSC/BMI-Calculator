1105c22fe889d3e3f81641b9fb54b4f75245d42a
Salitha Chathuranga <salithachathuranga94@gmail.com>
2018-10-24 14:32:43 +0530

    Add UI modifications

--- src/components/Form.js
+++ src/components/Form.js
@@ -38,15 +38,15 @@ class Form extends Component {
                         <div className="col-lg-3"></div>
                         <div className="col-lg-6">
                             <div className="form-group row">
-                                <label className="col-sm-2 col-form-label text-right">Weight</label>
+                                <label className="col-sm-2 col-form-label text-left">Weight</label>
                                 <div className="col-sm-10">
-                                    <input type="text" className="form-control" id="weight" name="weight" onChange={this.handleWeightChange} placeholder="Enter Your Weight"/>
+                                    <input type="text" className="form-control" id="weight" name="weight" onChange={this.handleWeightChange} placeholder="Enter Your Weight in kilograms"/>
                                 </div>
                             </div>
                             <div className="form-group row">
-                                <label className="col-sm-2 col-form-label text-right">Height</label>
+                                <label className="col-sm-2 col-form-label text-left">Height</label>
                                 <div className="col-sm-10">
-                                    <input type="text" className="form-control" id="height" name="height" onChange={this.handleHeightChange} placeholder="Enter Your Height"/>
+                                    <input type="text" className="form-control" id="height" name="height" onChange={this.handleHeightChange} placeholder="Enter Your Height in metres"/>
                                 </div>
                             </div>
                             <button type="submit" className="btn btn-primary btn-sm" onClick={this.getBMI} style={{float: 'left', marginLeft: '93px'}}>Get BMI</button>
